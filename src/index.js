fe237bd02c6b450d3f7bd997b73892c933b404c7
Tom Cornyn <cornyn@gmail.com>
2021-07-30 16:15:09 -0700

    Add Menu component.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './list.css';
+import Menu from './Menu';
+import App from './App'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Menu />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
