1b9d8fa21069e601ec715e5f2fa2005e9272095a
Tom Cornyn <cornyn@gmail.com>
2021-08-15 12:58:54 -0700

    Update HTML structure.

--- src/Demo.js
+++ src/Demo.js
@@ -1,44 +1,22 @@
 import React, {Component} from "react";
-import Data from './data.js';
 import './Demo.css';
-import { Button } from '@material-ui/core';
-
-const items = Data.work;
-
-const getData = info =>
-  Array.from({length: info.length}, (v, k) => k).map(k => ({
-      id: `item-${k}`,
-      title: `${info[k].title}`,
-      content: `${info[k].content}`,
-      url: `${info[k].url}`
-    }
-  ));
-
-const listItems = items.map((item) =>
-    <li key={item.guid}>
-      {item.title} | {item.content} | {item.url}
-    </li>
-);
+import '@fontsource/roboto';
+import "@fontsource/material-icons";
+import SaveButton from './SaveButton';
+import Menu from './Menu';
+import List from './List';
 
 class Demo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      items: getData(Data.work)
-    }
-  };
-
   render() {
     return <React.Fragment>
       <div>
-        <div>
-          <Button color="primary">Button</Button>
-        </div>
-        <div>
-          <ul>
-            {listItems}
-          </ul>
-        </div>
+        <SaveButton />
+      </div>
+      <div>
+        <Menu/>
+      </div>
+      <div>
+        <List />
       </div>
     </React.Fragment>
     }
