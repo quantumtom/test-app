97136cd23fd58f38574c6cd06277db9a8b6dbb96
Tom Cornyn <cornyn@gmail.com>
2021-08-19 13:31:51 -0700

    Add fonts.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '@fontsource/roboto';
+import "@fontsource/material-icons";
 import Demo from './Demo';
 import reportWebVitals from './reportWebVitals';
 
