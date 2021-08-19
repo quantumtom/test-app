23d8e33a2769ce807bf72a2caa7b2f1f109433d2
Tom Cornyn <cornyn@gmail.com>
2021-08-19 13:31:22 -0700

    Add h1 tag.

--- src/SaveButton.js
+++ src/SaveButton.js
@@ -1,10 +1,10 @@
 import React, {Component} from "react";
-import '@fontsource/roboto';
 import { Button } from '@material-ui/core';
 
 class SaveButton extends Component {
   render() {
     return <React.Fragment>
+      <h2>Save Button</h2>
       <Button color="primary">{"Click me!"}</Button>
     </React.Fragment>
   }
