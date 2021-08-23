0830ee902eb13a7fd24db26f1dc649e147bed2fc
Tom Cornyn <cornyn@gmail.com>
2021-08-23 11:35:11 -0700

    Remove H2.

--- src/SaveButton.js
+++ src/SaveButton.js
@@ -4,7 +4,6 @@ import { Button } from '@material-ui/core';
 class SaveButton extends Component {
   render() {
     return <React.Fragment>
-      <h2>Save Button</h2>
       <Button color="primary">{"Click me!"}</Button>
     </React.Fragment>
   }
