1295b1cc7664637965a0ae9590cb224ba28e59d1
Tom Cornyn <cornyn@gmail.com>
2021-10-23 14:51:48 -0700

    Update test.

--- src/Demo.test.js
+++ src/Demo.test.js
@@ -3,6 +3,6 @@ import Demo from './Demo';
 
 test('renders Demo', () => {
   render(<Demo />);
-  const demoElement = screen.getByText(/Demo Page/i);
+  const demoElement = screen.getByText(/Admin/i);
   expect(demoElement).toBeInTheDocument();
 });
