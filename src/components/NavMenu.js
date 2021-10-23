afe5216445e114b866574bc361b013ecf2d2fcc1
Tom Cornyn <cornyn@gmail.com>
2021-10-23 15:36:50 -0700

    Fix active menu nav link behaviour.

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -9,34 +9,31 @@ import "./NavMenu.css";
 
 class NavMenu extends Component {
   render() {
-    const handleSelect = (eventKey) => alert(`selected ${eventKey}`);
-
     return (
       <Navbar expand="sm" variant="light" bg="light" sticky="top" className="mb-3">
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Container>
             <Nav className="justify-content-center"
-              activeKey="link-1"
-              onSelect={handleSelect}
+              activeKey={window.location.pathname}
             >
               <Nav.Item>
-                <Nav.Link href="/commercials" eventKey="link-1" className="text-uppercase">Commercials</Nav.Link>
+                <Nav.Link href="/commercials" className="text-uppercase">Commercials</Nav.Link>
               </Nav.Item>
               <Nav.Item>
-                <Nav.Link href="/shorts" eventKey="link-2" className="text-uppercase">Shorts</Nav.Link>
+                <Nav.Link href="/shorts" className="text-uppercase">Shorts</Nav.Link>
               </Nav.Item>
               <Nav.Item>
-                <Nav.Link href="https://photo.marcflennert.com" eventKey='link-3' className="nav-link text-uppercase" target="_blank" rel="noopener">Photography</Nav.Link>
+                <Nav.Link href="https://photo.marcflennert.com" className="text-uppercase" target="_blank" rel="noopener">Photography</Nav.Link>
               </Nav.Item>
               <Nav.Item>
-                <Nav.Link href="/about" eventKey="link-4" className="text-uppercase">About</Nav.Link>
+                <Nav.Link href="/about" className="text-uppercase">About</Nav.Link>
               </Nav.Item>
               <Nav.Item>
-                <Nav.Link href="mailto:info@marcflennert.com" eventKey='link-4' className="nav-link text-uppercase">Contact</Nav.Link>
+                <Nav.Link href="mailto:info@marcflennert.com" className="text-uppercase">Contact</Nav.Link>
               </Nav.Item>
               <Nav.Item>
-                <Nav.Link href="/lists" eventKey='link-5' className="nav-link text-uppercase">Admin</Nav.Link>
+                <Nav.Link href="/lists" className="text-uppercase">Admin</Nav.Link>
               </Nav.Item>
             </Nav>
           </Container>
