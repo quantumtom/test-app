643da4099aeb37442eed57ed49d44c3c73ce6bf8
Tom Cornyn <cornyn@gmail.com>
2021-10-24 10:19:15 -0700

    Refine page layout and visual style.

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,8 @@
 import React, {Component} from "react";
 import Container from "react-bootstrap/Container";
 import Figure from "react-bootstrap/Figure";
-import portraitImageFile from "../portrait-medium.jpg";
+import portraitSmall from "../portrait-small.jpg";
+import portraitMedium from "../portrait-medium.jpg";
 import "./About.css";
 
 class About extends Component {
@@ -9,9 +10,9 @@ class About extends Component {
     return <React.Fragment>
       <main>
         <article>
-          <Container fluid>
+          <Container>
             <Figure>
-              <Figure.Image className="float-left mr-5" fluid rounded src={portraitImageFile} id="profile" alt="Marc Flennert" />
+              <Figure.Image className="float-left mr-5 profile-image float-left" fluid rounded src={portraitSmall} srcSet={`${portraitSmall} 150w, ${portraitMedium} 300w`} id="profile" alt="Marc Flennert" />
               <Figure.Caption className="text-start bodycopy prose-text float-left">
                 A lifelong photographer, Marc Flennert's passion is the practice of cinematography. With the combination of his natural creativity and technical know how, he's able to infuse his approach with a style of his own. Over the years he's gained vast experience working hand in hand with award winning directors, producers and agencies servicing major brands. Flennert continues to refine his talents by continually immersing himself in an array of projects around the world.
               </Figure.Caption>
