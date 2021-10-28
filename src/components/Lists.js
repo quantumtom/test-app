e55268b13ca3c9ec505b41252b71e6a58f97dbf4
Tom Cornyn <cornyn@gmail.com>
2021-10-28 13:14:48 -0700

    Remove disused file.

--- src/components/Lists.js
+++ src/components/Lists.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import Tab from "react-bootstrap/Tab";
 import Tabs from "react-bootstrap/Tabs";
 import Reorderer from "./Reorderer";
-import "./Lists.css"
 
 function Lists() {
   const [key, setKey] = useState('commercials');
