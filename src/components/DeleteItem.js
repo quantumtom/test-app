dc876a64175e30937ec5e022b9a7507b1a70bd6f
quantumtom <cornyn@gmail.com>
2022-01-27 14:08:54 -0800

    Swap out window globals for environment variables.

--- src/components/DeleteItem.js
+++ src/components/DeleteItem.js
@@ -5,7 +5,7 @@ import { Modal, Button, ButtonGroup }  from "react-bootstrap";
 import { default as axios } from "axios";
 import {FcFullTrash } from "react-icons/fc";
 
-axios.defaults.baseURL = window.API_BASE;
+axios.defaults.baseURL = process.env.REACT_APP_API_BASE;
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 class DeleteItem extends React.Component {
